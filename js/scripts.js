30ef1477aae23184a08f3e2c83f0dc292bb7e9cf
Keely Glenn and Minh Phuong <keely@>
2017-01-12 14:13:09 -0800

    FINISH ALGORITHM WITH MATH on JS we rock so much

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-    var a = parseInt($("input#side1").val());
-    var b = parseInt($("input#side2").val());
-    var c = parseInt($("input#side3").val());
+    var a = parseFloat($("input#side1").val());
+    var b = parseFloat($("input#side2").val());
+    var c = parseFloat($("input#side3").val());
+    var d = Math.max(a,b,c);
+    var e = (a + b + c) - d;
 
-    if (a = b = c) {
-      $("#tri").text("equilateral");
-      $(".answer").show();
-    // } else if (a = b != c|| b = c != a|| a = c != b) {
-    //   $("#tri").text("isosceles");
-    //   $(".answer").show();
-  } else if (a != b != c) {
-      $("#tri2").text("scalene");
-      $(".answer2").show();
-    // } else if (a + b <= c || b + c <= a || a + c <= b ) {
-    //   $("#tri").text("not a triangle");
-    //   $(".answer").show();
+    if ( e >= d ) {
+      if ( a === b && a === c) {
+        $("#tri").text("equilateral");
+        $(".answer").show();
+
+      } else if ( a === b && a === (e/2) || a === c && c === (e/2) || b === c && c === (e/2)) {
+        $("#tri").text("isosceles");
+        $(".answer").show();
+
+      } else {
+        $("#tri").text("not a triangle");
+        $(".answer").show();
+      }
+
+      event.preventDefault();
     }
-    event.preventDefault();
   });
 });
